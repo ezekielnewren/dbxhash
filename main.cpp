b21ac37796f3d29ffda98a7c938bcb9f5bb17230
Ezekiel Newren <ezekielnewren@gmail.com>
2021-06-23 21:13:24 -0600

    single thread dropbox content hash works

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 #include <cstdint>
 #include <fstream>
+#include <string>
 
+#include <boost/algorithm/hex.hpp>
 #include <boost/filesystem.hpp>
+#include <boost/system/error_code.hpp>
 
 using std::cout;
+using std::cerr;
 using std::endl;
+using std::string;
 
 namespace fs = boost::filesystem;
 
@@ -22,27 +27,40 @@ bool hashblock(byte* hash, byte* block, uint32_t size) {
 }
 
 int main(int argc, char** argv) {
-    if (argc < 1) {
+    if (argc <= 1) {
         cout << "not enough arguments" << endl;
+        return 1;
     }
 
     byte buff[4*1024*1024];
     byte hash[32];
 
+
+
     SHA256_CTX sha256;
     SHA256_Init(&sha256);
 
+    boost::system::error_code ec;
+    fs::path p = fs::current_path();
+    cout << p << endl;
 
-
-    std::ifstream file(argv[1]);
+    fs::ifstream file((fs::path(argv[1])));
+    if (! file.is_open()) {
+        cerr << "failed to open file" << endl;
+    }
     while (true) {
-        file.read((char*) buff, sizeof(buff));
-        std::streamsize read = file.gcount();
+        int amount = sizeof(buff);
+        std::streamsize read = file.readsome((char*) buff, amount);
+//        file.read((char*) buff, amount);
+//        std::streamsize read = file.gcount();
         if (read <= 0) break;
         hashblock(hash, buff, read);
+        cout << boost::algorithm::hex(string((const char*) hash, 32)) << endl;
         SHA256_Update(&sha256, hash, sizeof(hash));
     }
     SHA256_Final(hash, &sha256);
+    cout << boost::algorithm::hex(string((const char*) hash, 32)) << endl;
+
 
     file.close();
 
